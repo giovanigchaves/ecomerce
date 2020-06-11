ddacfe3601525df837527237f76aca83788e0461
Giovani G Chaves <Giovanigchaves.engenharia@gmail.com>
2020-06-11 13:55:45 -0300

    tela de login

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
-<?php 
-
-
+<?php
+session_start();
 require_once("vendor/autoload.php");
+//require_once("functions.php");
 
+use Hcode\Model\User;
 
 $app = new \Slim\Slim();
 
@@ -18,12 +19,42 @@ $app->get('/', function() {
 
 $app->get('/admin', function() {
 
+    User::verifyLogin();
+
     $page = new Hcode\PageAdmin();
 
     $page->setTpl("index");
 
 });
 
-$app->run();
+$app->get('/admin/login', function() {
 
+    $page = new Hcode\PageAdmin([
+        "header"=>false,
+        "footer"=>false
+    ]);
+
+    $page->setTpl("login");
+
+});
+
+$app->post('/admin/login', function() {
+
+    User::login($_POST['deslogin'], $_POST['despassword']);
+
+    header("Location: /admin");
+    exit;
+
+});
+
+$app->get('/admin/logout', function() {
+
+    User::logout();
+
+    header("Location: /admin/login");
+    exit;
+
+});
+
+$app->run();
  ?>
